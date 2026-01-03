cddd96a43964bd68fa698898a32c2df13e5b6be8
Piponsio013 <pipe1910@live.com.mx>
2026-01-03 01:21:00 -0600

    Initial commit

---
+++ src/main/java/io/github/piponsio/smartfinances_api/SmartfinancesApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.piponsio.smartfinances_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartfinancesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartfinancesApiApplication.class, args);
+	}
+
+}
