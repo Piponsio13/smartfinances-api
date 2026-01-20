5f67d09b0e3d2fc963d3daf04f2c20e639f12177
Piponsio013 <pipe1910@live.com.mx>
2026-01-19 22:40:10 -0600

    chore: remove unused import

--- src/main/java/io/github/piponsio/smartfinances_api/controller/CategoryController.java
+++ src/main/java/io/github/piponsio/smartfinances_api/controller/CategoryController.java
@@ -2,8 +2,6 @@ package io.github.piponsio.smartfinances_api.controller;
 
 import java.util.List;
 
-import javax.print.DocFlavor.STRING;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
