f9a0d5086f2e91fcd6feb4345998091976bdbe94
Piponsio013 <pipe1910@live.com.mx>
2026-01-21 19:51:31 -0600

    Fixed JPA error

--- src/main/java/io/github/piponsio/smartfinances_api/repository/TransactionRepository.java
+++ src/main/java/io/github/piponsio/smartfinances_api/repository/TransactionRepository.java
@@ -1,5 +1,6 @@
 package io.github.piponsio.smartfinances_api.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import io.github.piponsio.smartfinances_api.entity.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    Optional<Transaction> findByID(Long id);
+    List<Transaction> findByUserId(Long userId);
 
     Optional<Transaction> findByIdAndUserId(Long id, Long userId);
 }
